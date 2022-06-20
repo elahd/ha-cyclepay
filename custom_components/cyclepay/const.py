de515e0954f48119c6d04a7114beefa73815937b
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2022-06-20 01:33:24 +0000

    updated consts

--- custom_components/cyclepay/const.py
+++ custom_components/cyclepay/const.py
@@ -6,7 +6,38 @@ INTEGRATION_NAME = "CyclePay for ESD/Hercules Laundry Rooms"
 
 STARTUP_MESSAGE = f"""
 ===================================================================
-CYCLEPAY
+
+                                       o  @o**o#@@@
+                                      O *#o°°°O@ O
+                                     o oOo°°*O@ @
+                       @@@@@@@@@@@ @o oO*°°*O@ @
+                      O              o#O***O@
+                    @  @          @ OOOO#@@  @ @@
+                   @  #ooooooooooooooo*****oo**#@@@
+                  @ o#*°°°°°°°°°°°°°°°°°°°°°°°o@ @
+                 @ o#*°°°°°°°°°°°°°°°°°°°°°°°O@ o
+               @# oO*°°°°°°°°°°°°°°°°°°°°°°*O@ @ @
+              @@ oO*°°°°°°°****************O@
+             *@ OO*°°°°°°°OOOOOOOoOOOOOO#@@@ @@@@@@
+            *@ OO°°°°°°°°*o***o# #o***********o*o#@@@
+           *  OO°°°°°°°°°°°°°*#o#o°°°°°°°°°°°°°°O@ O
+          O  OO°°°°°°°°°°°°°o@oOo°°°°°°°°°°°°°*O@ @
+         * .OO°°°°°°°°°°°°°o@oO*°°°°°°°°°°°°°*#@ @
+        * *O#*************o#o#o*****°°°°°°°°*#@ @
+       @@ooOoOOOOOOOOOOO#@@OOOOOOOOo°°°°°°°*#@ @
+         oo°**°****°*. #o**o*******°°°°°°°*#  @
+                     o#o°°°°°°°°°°°°°°°°°*# °@
+          @@@@@@@@o oOo°°°°°°°°°°°°°°°°°o# .@
+                 O oO*°°°°°°°°°°°°°°°°°o# o@
+                O oO*°°***************o# o@
+               @ oO*°°*OOOOOOOOOOOOO#@@ °
+             @@ oO*°°*##°°°°°°°°°°°    O
+            *@ OO*°°o#              o@@
+           *@ OO°°°o@ *@ @@@@@@@@@@
+          *  OO°°°o@ o
+         @ *O#***o@ O
+        @*ooOOO#@@ *
+
 {DOMAIN}
 This is a custom component
 If you have any issues with this you need to open an issue here:
